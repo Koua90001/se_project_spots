81053846b779e1b9887d7b23981c06b372c190b4
KKO <oumarkouakou@yahoo.com>
2024-09-10 11:01:15 -0400

    Sprint 4 final project

--- scripts/index.js
+++ scripts/index.js
@@ -23,15 +23,12 @@ const cardTemplate = document.querySelector("#card-template");
 const cardsList = document.querySelector(".cards__list");
 
 function getCardElement(data) {
-  console.log(data);
   const cardElement = cardTemplate.content.querySelector(".card").cloneNode(true);
   const cardNameEl = cardElement.querySelector(".card__title");
   const cardImage = cardElement.querySelector(".card__image");
 
   cardNameEl.textContent = data.name;
-  let userTemplate = document.querySelector("#card-template").content;
-  let usersOnline = document.querySelector(".card");
-  let userElement = userTemplate.querySelector(".card").cloneNode(true);
+
 
   cardImage.src = data.link
   cardImage.alt = data.name
